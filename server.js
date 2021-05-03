67aef576253baa7e2f51145d908c22ba358bfc38
dipu-d <dipu234dahal@gmail.com>
2021-05-04 00:54:28 +0545

    Solved an error

--- server.js
+++ server.js
@@ -21,15 +21,14 @@ app.set("view engine", "ejs");
 
 const { User, Report, Message, Chat } = require(__dirname + "/schema.js");
 const emailContent = fs.readFileSync(
-  __dirname + "/views/reset_password_email.ejs",
-  "utf8"
+  __dirname + "/views/reset_password_email.ejs"
 );
 
 const serviceAccount = {
   type: "service_account",
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -300,11 +299,11 @@ connection.once("open", () => {
       });
   });
 
-
   app.get("/uploads/:fileId", (req, res) => {
-    res.redirect(process.env.FIREBASE_BASE_IMAGE_URL + req.params.fileId + "?alt=media");
+    res.redirect(
+      process.env.FIREBASE_BASE_IMAGE_URL + req.params.fileId + "?alt=media"
+    );
   });
-
 
   app.post("/login", (req, res) => {
     req.body.email = req.body.email.toLowerCase();
